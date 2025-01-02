b09b162e3041cfb5c07c5dbb2d0f097bfe64eee9
khanaleena750 <148544727+khanaleena750@users.noreply.github.com>
2025-01-02 15:46:08 +0400

    Delete src directory

--- src/index.js
+++
@@ -1,7 +0,0 @@
-// src/index.js
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './styles.css';  // Make sure this matches the filename
-import App from './App';
-
-ReactDOM.render(<App />, document.getElementById('root'));
